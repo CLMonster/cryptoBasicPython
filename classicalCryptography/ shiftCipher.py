1e4b7b818b90ad2549a376fb54f2a92504a260db
wagndebao <424546210@qq.com>
2023-01-16 20:52:00 +0800

    update

--- classicalCryptography/ shiftCipher.py
+++ classicalCryptography/ shiftCipher.py
@@ -14,23 +14,23 @@ def shift_enc(plain_text, key):
 
     return cipher_text
 
-
 def shift_dec(cipherText, key):
-  ciphertext = list(cipherText)
-  plaintext = []
+    """
+    shift crypto decryption
+    """
+    ciphertext = list(cipherText)
+    plaintext = []
   
-  for i in ciphertext:
-    # print(i)/
-    a = ord(i)
-    if a >= 97 and a <= 122:
-      plaintext.append(chr(((a - 97 - key) % 26) + 97))
-    elif a >= 65 and a <= 90:
-      plaintext.append(chr(((a - 65 - key) % 26) + 65))
-    else :
-      plaintext.append(i)
+    for i in ciphertext:
+        a = ord(i)
+        if a >= 97 and a <= 122:
+            plaintext.append(chr(((a - 97 - key) % 26) + 97))
+        elif a >= 65 and a <= 90:
+            plaintext.append(chr(((a - 65 - key) % 26) + 65))
+        else :
+            plaintext.append(i)
 
-  return plaintext
-
+    return plaintext
 
 if __name__ == "__main__":
   # plText = "TMHENQL NBDZM DQZ EQDPTDMS"
