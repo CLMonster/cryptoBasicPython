a0fceef33f25c3fe22cd6e1705ff52e11dd3fa2c
wagndebao <424546210@qq.com>
2023-01-16 20:49:43 +0800

    fixed a bug: round bracket problem line 26

--- classicalCryptography/ shiftCipher.py
+++ classicalCryptography/ shiftCipher.py
@@ -23,15 +23,19 @@ def shift_dec(cipherText, key):
     # print(i)/
     a = ord(i)
     if a >= 97 and a <= 122:
-      plaintext.append(chr((a - 97 - key) % 26) + 97)
+      plaintext.append(chr(((a - 97 - key) % 26) + 97))
     elif a >= 65 and a <= 90:
       plaintext.append(chr(((a - 65 - key) % 26) + 65))
     else :
       plaintext.append(i)
+
   return plaintext
 
 
 if __name__ == "__main__":
-  plText = "TMHENQL NBDZM DQZ EQDPTDMS"
-  for i in range(1,26):
-    print("".join(shift_dec(plText, i)))
+  # plText = "TMHENQL NBDZM DQZ EQDPTDMS"
+    plText = input("please enter a plaintext:\n")
+#   print(plText, type(plText))
+    print("============result=============")
+    for i in range(0,26):
+        print("".join(shift_dec(plText, i)))
